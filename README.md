fbf3fb7708ee7e5feea4e37e0c26b0826d4891e8
Gareth Widlansky <101901964+gerblesh@users.noreply.github.com>
2025-08-03 17:16:02 -0700

    add files

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # modeline.hx
 Helix Plugin for modeline
+
+
+# Installation
+Currently, you need to use a custom branch in order to have access to regex matching on ropes from steel, building and installing helix from a custom branch (not the original plugin PR):
+```sh
+git clone https://github.com/gerblesh/helix.git -b line-number-config
+```
+
+then build/install the helix fork with:
+```sh
+cargo xtask steel
+```
+
+to install the plugin with forge:
+```sh
+forge pkg install --git https://github.com/gerblesh/modeline.hx.git
+```
+
+add the lines to your scm file to configure the modeline
+
+```scheme
+(require "modeline/modeline.scm")
+
+;; register the modeline to automatically run on save
+(modeline-enable)
+
+;; this steel function refreshes the modeline, you can bind a key to call it manually in your helix/init.scm or config.toml
+(refresh-modeline)
+```
+
+supports very basic emacs and vim modelines with some caveats:
+
+
+supported modeline fields:
+et, expand-tab: use spaces for indent
+noet, no-expand-tab: use tabs for indent (tab width not supported)
+tab-width, sw, ts, shiftwidth: tab width (only affects spaces)
+language, mode, ft, filetype: language/syntax highlighting, only some filetype aliases are supported and may vary
+indent-tabs-mode: can either be "nil" (spaces) or "t" (tabs)
+
+example modelines:
+```py
+# vim: set ts=4 sw=4 et: ft=py
+```
+
+```py
+# -*- mode: py; tab-width: 4; indent-tabs-mode: nil -*-
+```
---
+++ modeline.scm
@@ -0,0 +1,114 @@
+;; unfortunately, setting the tab-width and using the \t character is not currently supported (requires a patch to allow a cmd to set the tab character width)
+;; This *already* requires a patch that adds rope->match-regexp
+;; -*- language: scm; tab-width: 2; indent-tabs-mode: nil -*-
+
+(require-builtin helix/core/text)
+(require (prefix-in helix. "helix/commands.scm"))
+(require "helix/editor.scm")
+
+(define lang-aliases
+  (hash "js"
+        "javascript"
+        "ts"
+        "typescript"
+        "sh"
+        "bash"
+        "rb"
+        "ruby"
+        "py"
+        "python"
+        "c++"
+        "cpp"
+        "cxx"
+        "cpp"
+        "h"
+        "c"
+        "hpp"
+        "cpp"
+        "el"
+        "elisp"
+        "rkt"
+        "racket"
+        "scm"
+        "scheme"
+        "md"
+        "markdown"
+        "txt"
+        "text"
+        "yml"
+        "yaml"
+        "rs"
+        "rust"))
+
+(define emacs-modeline-regex "-\\*-\\s*(.*?)\\s*-\\*-")
+(define vim-modeline-regex "(?i)(?:vi|vim):.*?((?:set)?\\s+[^:]*).*")
+
+(define (split-whitespace s)
+  (rope->match-regexp (string->rope s) "[^\\s;:.,()\\[\\]{}=]+"))
+
+(define (check-modeline line)
+  (or (let ([m (try-list-ref (rope->match-regexp line emacs-modeline-regex) 0)])
+        (and m (split-whitespace m)))
+      (let ([m (try-list-ref (rope->match-regexp line vim-modeline-regex) 0)])
+        (and m (split-whitespace m)))))
+
+(define (search-modelines t)
+  (let* ([line-count (rope-len-lines t)]
+         [max-lines (min 5 line-count)]
+         [check-lines (lambda (start count)
+                        (let loop ([i start])
+                          (when (< i (+ start count))
+                            (let ([line (rope->line t i)])
+                              (let ([parsed (check-modeline line)])
+                                (if parsed
+                                    (apply-modeline parsed)
+                                    parsed)))
+                            (loop (+ i 1)))))])
+    (or (check-lines 0 max-lines) (check-lines (- line-count max-lines) max-lines))))
+
+(define (modeline-enable)
+  (register-hook! 'document-opened modeline)
+  (register-hook! 'document-saved modeline))
+
+(define (get-current-doc-id)
+  (let* ([focus (editor-focus)]) (editor->doc-id focus)))
+
+(define (modeline doc-id)
+  (let ([t (editor->text doc-id)]) (search-modelines t)))
+
+(define (refresh-modeline)
+  (modeline (get-current-doc-id)))
+
+(define (normalize-lang s)
+  (or (hash-try-get lang-aliases s) s))
+
+;; common keys used in emacs and vim modelines:
+(define tabs-mode (hash "nil" #f "t" "t"))
+(define language-keys (hashset "mode" "language" "ft" "filetype"))
+(define width-keys (hashset "tab-width" "sw" "ts" "shiftwidth"))
+(define tab-keys (hashset "noet" "no-expand-tab"))
+(define expand-tab-keys (hashset "et" "expand-tab"))
+
+(define (apply-modeline lst)
+  (let loop ([i 0]
+             [indent #f])
+    (when (< i (length lst))
+      (when indent
+        (helix.indent-style indent))
+      (let* ([current (try-list-ref lst i)]
+             [next (try-list-ref lst (+ i 1))])
+        (cond
+          [(and next (hashset-contains? language-keys current))
+           (begin
+             (helix.set-language (normalize-lang next))
+             (loop (+ i 2) indent))]
+          [(and next (hashset-contains? width-keys current) (string->number next) (bool? indent))
+           (loop (+ i 2) next)]
+          [(and (not indent) (hashset-contains? expand-tab-keys current)) (loop (+ i 1) "2")]
+          [(hashset-contains? tab-keys current) (loop (+ i 1) "t")]
+          [(and (string=? current "indent-tabs-mode") next)
+           (loop (+ i 2) (let ([m (hash-ref tabs-mode next)]) (if m m indent)))]
+          [else (loop (+ i 1) indent)])))))
+
+(provide modeline-enable
+         refresh-modeline)
